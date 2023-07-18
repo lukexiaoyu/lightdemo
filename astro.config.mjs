ded2c89139afbbc6ad5018efc0a1463847fe34af
lukexiao <814008000@163.com>
2023-07-18 20:49:34 +0800

    11

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,47 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'My Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-    }),
-  ],
-
+  integrations: [starlight({
+    title: 'KL palace',
+    social: {
+      github: 'https://github.com/withastro/starlight',
+      baidu: 'https://www.baidu.com'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }, {
+      label: 'wow',
+      autogenerate: {
+        directory: 'wow'
+      }
+      
+    },
+    {label:'svelte组件库',
+      autogenerate:{
+        directory:'svelte'
+      }
+    }]
+  }), svelte(), tailwind()],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
